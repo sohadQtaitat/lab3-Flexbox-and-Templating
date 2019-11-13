36b1df71508ffecaf453b256091cd966fb178a76
sohadQtaitat <qtaitat3@gmail.com>
2019-11-13 17:15:42 +0200

    not comblete

--- js/app.js
+++ js/app.js
@@ -1,64 +1,134 @@
 'use strict';
-function Horn(data) {
-    this.image_url = data.image_url;
-    this.title = data.title;
-    this.horns = data.horns;
-    this.description = data.description;
-    this.keyword = data.keyword;
-    Horn.all.push(this);
-}
-Horn.all = [];
 
-
-
-Horn.prototype.render = function () {
-    let eachHorn = $('<div></div>');
-    eachHorn.addClass(this.keyword);
-    let template = $('#photo-template').html();
-    eachHorn.html(template);
-
-    eachHorn.find('h2').text(this.title);
-    eachHorn.find('img').attr('src', this.image_url);
-    eachHorn.find('p').text(this.description);
-
-    $('main').append(eachHorn);
-
-}
-
-function selectBox() {
-    let select = $('select');
-    let seen = {};
-    Horn.all.forEach((horn) => {
-        if (!seen[horn.keyword]) {
-
-            let option = `<option value="${horn.keyword}">${horn.keyword}</option>`;
-            select.append(option);
-            seen[horn.keyword] = true;
-        }
-
-    });
+function Horn(horn) {
+  this.title = horn.title;
+  this.image_url = horn.image_url;
+  this.description = horn.description;
+  this.keyword = horn.keyword;
+  this.horns = horn.horns;
+  keyArr.push(this.keyword);
 }
 
 
 
-$('select').on('change', function () {
-    let selected = $(this).val();
-    $('div').hide();
-    $(`.${selected}`).fadeIn(800);
+
+
+Horn.allHorns = [];
+let keyArr = [];
+console.log(keyArr);
+let uniqeArr = [];
+Horn.prototype.render = function() {
+  const $template = $('#hornsTem').html();
+  const $source = Handlebars.compile($template);
+  return $source(this);
+};
+
+
+
+
+var page = 'data/page-1.json';
+
+
+var page = 'data/page-2.json';
+
+
+
+
+
+function makeNewarr() {
+  var newArr = [...new Set(keyArr)];
+  uniqeArr.push(newArr);
+}
+
+
+
+let selectRend = () => {
+  uniqeArr[0].forEach( element => {
+    $('select').append('<option class="optClone"></option>');
+    console.log(element);
+    let optionClone = $('option[class="optClone"]');
+    optionClone.text(element);
+    optionClone.removeClass('optClone');
+  });
+};
+
+
+
+
+
+
+
+
+Horn.readJson = (page) => {
+  $.get(page, 'json')
+    .then(data => {
+      data.forEach(obj => {
+        Horn.allHorns.push(new Horn(obj));
+      });
+    })
+    .then(Horn.loadHorns).then(makeNewarr).then(selectRend);
+};
+
+
+
+
+Horn.loadHorns = () => {
+  Horn.allHorns.forEach(horn => {
+    $('#photo-template').append(horn.render());
+  });
+};
+
+$(() => Horn.readJson(page));
+
+$('select').on('change',selShow);
+function selShow(){
+  let selItem=$(this).val();
+  $('div').hide();
+  $('div[id = "'+selItem+'"]').show();
+}
+
+$('#pg2').click(function(){
+  page = 'data/page-2.json';
+  $('div').remove();
+  $('option').remove();
+  Horn.allHorns = [];
+  uniqeArr = [];
+  keyArr = [];
+  $(() => Horn.readJson(page));
 });
 
+$('#pg1').click(function(){
+  page = 'data/page-1.json';
+  $('div').remove();
+  $('option').remove();
+  Horn.allHorns = [];
+  uniqeArr = [];
+  keyArr = [];
+  $(() => Horn.readJson(page));
+});
 
+$('#titleSort').click(function(){
+  $('div').remove();
+  uniqeArr[0] = [];
+  Horn.allHorns.sort(function(a, b) {
+    return a.title.localeCompare(b.title);
+  });
+  $('select').remove();
+  $('#pg1').remove();
+  $('#pg2').remove();
+  Horn.loadHorns();
+});
 
+$('#numberSort').click(function(){
+  $('div').remove();
+  uniqeArr[0] = [];
+  Horn.allHorns.sort( function ( a, b ) { return b.horns - a.horns; } );
+  $('select').remove();
+  $('#pg1').remove();
+  $('#pg2').remove();
+  Horn.loadHorns();
+});
 
-$.get('../data/page-1.json').then(data => {
-    data.forEach(items => {
-        let horn = new Horn(items);
-
-        horn.render();
-
-    });
-    console.log(data);
-})
-    .then(() => {
-        selectBox();
-    });
+$('#home').click(function(){
+  location.reload();
+});
